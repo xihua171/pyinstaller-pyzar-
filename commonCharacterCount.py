ebf0283f33f254f4350e26e03f0725ad1a1bf75e
xihua171 <35020471+xihua171@users.noreply.github.com>
2019-06-18 08:33:49 +0800

    Delete commonCharacterCount.py

--- commonCharacterCount.py
+++
@@ -1,9 +0,0 @@
-def commonCharacterCount(s1, s2):
-    s1 = sorted(s1)
-    s2 = sorted(s2)
-    z = 0
-    for i3 in s1:
-        if i3 in s2:
-            z = z+1
-            s2.remove(i3)
-    return (z)
